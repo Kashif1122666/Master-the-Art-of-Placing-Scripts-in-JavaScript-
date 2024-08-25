8e4ddb92c3dde225cbd2f45351d048021f3ea13a
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-25 13:10:22 -0700

    Add files via upload
    Done with  Master the Art of Placing Scripts in JavaScript!

---
+++ index.js
@@ -0,0 +1,53 @@
+// Chapter -43: Placing Scripts  
+
+// Where To Place/Write Javascript code?
+
+// the <script> tag 
+// In Html, Javascript code is inserted betwween <script> and  </script> tags. 
+
+// Javascript in <head> or <body>
+// you can place any number of scripts in an Html document.
+// scripts can be placed in the <body> , or in the <head> section of an Html page, or
+// in both.
+
+// Javascript in <head> (demonstrate in html file)
+// In this example , a Javascript function is placed in the <head> section of an html page.
+// The function is invoked(called) when a button is clicked.
+
+// Javascript in <body> (demonstrate in html file)
+// In this example , a Javascript function is placed in the <head> section of an html page.
+// The function is invoked(called) when a button is clicked.
+
+// Javascript in <body> (demonstrate in html file)
+// In this example , a JavaScript function is placed in the <head> section of an Html page.
+// The  function is invoked(called) when a button is clicked.
+
+// JavaScript  in <body> (demonstrate in html file)
+// In  this example , a Javascript function is placed in the <body> section of an html page.
+// The function is invoked(called) when a button is clicked.
+
+// Note:
+// Placing script at the bottom of the <body> element improves the display speed,
+// because script interpretation slows down the display.
+
+// Externale javascript (demonstrate in this file)
+// scripts can also be placed in external files.
+// function helloFromExternalFile() {
+//     alert("hello From External File");
+// }
+
+// External scripts are practical when the same code is used in many different web pages.
+// To use an external script , put the name of the script file in the src(source) attribute
+// of a <script> tag. 
+
+// you can place an external script reference in <head> or <body> as you like.
+// The script will behave as if it was located exactly where the <script> tag is located.
+
+// Note:
+// External JavaScripts cannot contain <script> tags.
+
+// External javaScript Advantages
+// It seperats html and Javascript
+// It makes html and Javascript easier to read and maintain
+// Cached JavaScript files can speed up page laods
+
